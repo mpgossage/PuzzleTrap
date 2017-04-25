6be22e0b556b70ec9e122aeafb519bc05580ff3d
MarkGossage <mpgossage@yahoo.co.uk>
2017-04-25 20:03:40 +0100

    Added A* solver
    Updated solver to use A*, also fixed stupid mistake in level34

--- Solver/Program.cs
+++ Solver/Program.cs
@@ -9,10 +9,57 @@ namespace Solver
 {
     class Program
     {
+        readonly string rootDir = Path.Combine(Directory.GetCurrentDirectory(), "../../../Solver/levels");
+
         static void Main(string[] args)
         {
-            string rootDir = Path.Combine(Directory.GetCurrentDirectory(), "../../../Solver/levels");
-            string file = File.ReadAllText(Path.Combine(rootDir, "level034.txt"));
+            //new Program().SolveAStar("level016.txt");
+            new Program().SolveAStar("level036.txt");
+            //new Program().SolveAll();
+        }
+
+        void SolveAll()
+        {
+            Solver solver = new Solver();
+            System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+            for (int i=1;i<=33;i++)
+            {
+                string file = File.ReadAllText(Path.Combine(rootDir, string.Format("level{0:000}.txt",i)));
+                LevelState ls = LevelState.FromString(file);
+                timer.Restart();
+                string[] result = solver.SolveAStar(ls);
+                timer.Stop();
+                if (result==null)
+                {
+                    Console.WriteLine("Unable to solve level {0} in {1} ms", i,timer.ElapsedMilliseconds);
+                }
+                else
+                {
+                    Console.WriteLine("Solved level {0} in {1} ms", i, timer.ElapsedMilliseconds);
+                }
+            }
+            Console.WriteLine("Test Complete");
+            Console.ReadLine();
+        }
+
+        void SolveAStar(string name)
+        {
+            string file = File.ReadAllText(Path.Combine(rootDir, name));
+            LevelState ls = LevelState.FromString(file);
+
+            Solver solver = new Solver();
+            string[] result = solver.SolveAStar(ls);
+            if (result != null)
+            {
+                Console.WriteLine("Solution Found:");
+                foreach (var s in result) Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+
+        void SolveOne(string name)
+        {
+            string file = File.ReadAllText(Path.Combine(rootDir,name));
             LevelState ls=LevelState.FromString(file);
 
             Solver solver = new Solver();
--- Solver/Solver.cs
+++ Solver/Solver.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Solver
 {
+    using Priority_Queue;
     using Position = System.Tuple<int, int>;
 
     public class Solver
@@ -96,6 +97,55 @@ namespace Solver
 
             }
             return null;
+        }
+
+        /** A Star Based Solver
+        */
+        public string[] SolveAStar(LevelState ls)
+        {
+            if (!ls.IsMiceLeft()) return new string[] { ls.ToString() };
+            var liveQueue = new SimplePriorityQueue<Solution>(); // items to try
+            var deadQueue = new HashSet<string>();  // states which we tried and didn't work
+
+            Solution sol = new Solution(ls);
+            liveQueue.Enqueue(sol, sol.fitness); // lower fitness = higher priority
+            int count = 0, totalSolutions = 0;
+            while (liveQueue.Count > 0)
+            {
+                var s = liveQueue.Dequeue();
+                string stateStr = s.ls.ToString();
+                if (count % 10000 == 0)
+                {
+                    Console.WriteLine("Step {0}, total Solutions {1}, liveList {2}, deadList {3} current State", count, totalSolutions, liveQueue.Count, deadQueue.Count);
+                    Console.WriteLine(stateStr);
+                }
+                count++;
+                deadQueue.Add(stateStr);    // this has been tried
+                foreach (var m in s.ls.GetPossibleMoves())
+                {
+                    totalSolutions++;
+                    var newLs = s.ls.MakeMove(m.Item2);
+
+                    // ignore existing solutions
+                    var newLsStr = newLs.ToString();
+                    if (deadQueue.Contains(newLsStr)) continue;
+                    // should compare against the live list too...
+                    // this makes a BIG difference on level 16
+                    if (liveQueue.FirstOrDefault((sln) => { return sln.SimilarTo(newLsStr); }) != null)
+                        continue;
+                    
+                    var newSol = new Solution(newLs, m.Item1, s.ls.GetCell(m.Item1), s.moves.ToArray());
+
+                    if (!newLs.IsMiceLeft())
+                    {
+                        Console.WriteLine("Found Solution on Step {0}, total Solutions {1}, liveList {2}, deadList {3} current State", count, totalSolutions, liveQueue.Count, deadQueue.Count);
+                        return newSol.moves.ToArray();   // solution found
+                    }
+                    liveQueue.Enqueue(newSol, newSol.fitness);  // add to queue
+                }
+            }
+            Console.WriteLine("Failed: tried {0} steps, {1} total solutions, {2} unique solutions", count, totalSolutions, deadQueue.Count);
+            return null; // not found
         }
 
         private class Solution
